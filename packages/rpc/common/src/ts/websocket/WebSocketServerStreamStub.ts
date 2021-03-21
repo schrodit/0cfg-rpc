3b60b57fb73d1eb7531ad50b08d10ef5c9d555c1
jonas <jonas@codesphere.cloud>
2021-03-21 13:18:17 +0100

    Prevents server streams from being started multiple times.

--- packages/rpc/common/src/ts/websocket/WebSocketServerStreamStub.ts
+++ packages/rpc/common/src/ts/websocket/WebSocketServerStreamStub.ts
@@ -1,5 +1,4 @@
 import {ServerStreamStub} from '../stub/ServerStreamStub';
-import {getOk} from '@0cfg/reply-common/lib/Reply';
 import {CommonReconnectingWebSocket} from '@0cfg/stubs-common/lib/messaging/CommonReconnectingWebSocket';
 import {Sequential} from '@0cfg/utils-common/lib/Sequential';
 import {WebSocketStreamStub} from './WebSocketStreamStub';
@@ -8,12 +7,17 @@ export class WebSocketServerStreamStub<ClientMessageT, ServerMessageT>
     extends WebSocketStreamStub<ClientMessageT, ServerMessageT>
     implements ServerStreamStub<ClientMessageT, ServerMessageT> {
 
+    private started: boolean = false;
+
     public constructor(socket: CommonReconnectingWebSocket, requestIdSequential: Sequential, method: string) {
         super(socket, requestIdSequential, method);
     }
 
     public start(message: ClientMessageT): void {
+        if (this.started) {
+            throw new Error('Trying to start a server stream that is already started.');
+        }
+        this.started = true;
         super.send(message);
-        this.complete(getOk());
     }
 }
